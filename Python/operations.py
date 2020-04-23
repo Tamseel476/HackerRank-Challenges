b0349650bb365b741294b593d6f3835c8dd27167
Tamseel Ahmed <tamseel476@gmail.com>
2020-04-24 03:51:36 +0400

    Update operations.py

--- Python/operations.py
+++ Python/operations.py
@@ -7,7 +7,7 @@ The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
 Input Format
 
-The first line contains the first integer, . The second line contains the second integer, .
+The first line contains the first integer, a. The second line contains the second integer, b.
 
 Constraints
 1 < a < 10^10

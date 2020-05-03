7fb9dcbd162b49cd9adb7f09d4efbe96fb40c94e
Tamseel Ahmed <tamseel476@gmail.com>
2020-05-03 08:54:37 +0400

    Create lists.py

---
+++ Python/lists.py
@@ -0,0 +1,87 @@
+'''
+
+Consider a list (list = []). You can perform the following commands:
+
+1. insert i e: Insert integer e at position i.
+2. print: Print the list.
+3. remove e: Delete the first occurrence of integer e.
+4. append e: Insert integer e at the end of the list.
+5. sort: Sort the list.
+6. pop: Pop the last element from the list.
+7. reverse: Reverse the list.
+
+Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. 
+Iterate through each command in order and perform the corresponding operation on your list.
+
+Input Format:
+
+The first line contains an integer,n, denoting the number of commands.
+Each line i of the n subsequent lines contains one of the commands described above.
+
+Constraints:
+The elements added to the list must be integers.
+
+Output Format:
+For each command of type print, print the list on a new line.
+
+Sample Input:
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print
+remove 6
+append 9
+append 1
+sort
+print
+pop
+reverse
+print
+
+Sample Output:
+
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+
+
+'''
+
+if __name__ == '__main__':
+    N = int(input())
+    hello = []
+    for _ in range(N):
+        z = input()
+        
+        if "insert" in z:
+            zlist = z.split(" ")
+            op1 = int(zlist[1])
+            op2 = int(zlist[2])
+            hello.insert(op1,op2)
+            
+        
+        if "print" in z:
+           print(hello)
+        
+        
+        if "remove" in z:
+            zlist = z.split(" ")
+            op1 = int(zlist[1])
+            hello.remove(op1)
+        
+        if "append" in z:
+            zlist = z.split(" ")
+            op1 = int(zlist[1])
+            hello.append(op1)
+
+        if "sort" in z:
+           hello.sort()
+        
+        if "pop" in z:
+            hello.pop(-1)
+
+        if "reverse" in z:
+           hello.reverse()
+        
